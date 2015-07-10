8107ec98497c4e2ca8fa81d2a2cf5e801e2020b9
Spencer Smitherman <spencer.smitherman@gmail.com>
2015-07-10 14:59:23 -0700

    Added HEB stuff

---
+++ heb_homepage/HEB Texas Grocery _ Here Everything's Better_files/serverComponent.php
@@ -0,0 +1,2 @@
+
+Bootstrapper._serverTime = '2015-07-10 21:56:16'; Bootstrapper._clientIP = '0.0.0.0'; var psj0 = 'https://nexus.ensighten.com/heb/prod/code/0deecde4300f7f4fff62466021a65a6f.js?conditionId0=242723';Bootstrapper.insertScript(psj0);var psj1 = 'https://nexus.ensighten.com/heb/prod/code/1afce12ef37f17f76bba5b4a5bc0c52b.js?conditionId0=242769';Bootstrapper.loadScriptCallback(psj1, Bootstrapper.callOnPageSpecificCompletion);Bootstrapper.setPageSpecificDataDefinitionIds([])
